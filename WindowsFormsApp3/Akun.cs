dc7355e06f0dbcfbde1a5628c354e6fd43a982fa
BonarS <bonaruli42@gmail.com>
2022-12-22 21:04:46 +0700

    minor update

---
+++ WindowsFormsApp3/Akun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    internal class Akun
+    {
+        //tiap login, data username sama notelp dari data base akan dipanggil
+        //masuk ke dalam new object. jadi tiap login bakal bikin new object
+        public string userName;
+        public string Password;
+    }
+}
