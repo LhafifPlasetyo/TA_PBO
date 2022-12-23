57fdf18c43854776831e8efd80123e8f03b14ea6
BonarS <bonaruli42@gmail.com>
2022-12-23 13:08:42 +0700

    update FormAdmin

--- WindowsFormsApp3/FormAdmin.Designer.cs
+++ WindowsFormsApp3/FormAdmin.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.register_button = new System.Windows.Forms.Button();
+            this.login_button = new System.Windows.Forms.Button();
+            this.Label_Password = new System.Windows.Forms.Label();
+            this.TxtBox_Password = new System.Windows.Forms.TextBox();
+            this.Label_UserName = new System.Windows.Forms.Label();
+            this.TxtBoxt_UserName = new System.Windows.Forms.TextBox();
+            this.RegisterAdmin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // register_button
+            // 
+            this.register_button.Location = new System.Drawing.Point(392, 346);
+            this.register_button.Name = "register_button";
+            this.register_button.Size = new System.Drawing.Size(128, 44);
+            this.register_button.TabIndex = 14;
+            this.register_button.Text = "Register";
+            this.register_button.UseVisualStyleBackColor = true;
+            // 
+            // login_button
+            // 
+            this.login_button.Location = new System.Drawing.Point(573, 343);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(167, 51);
+            this.login_button.TabIndex = 13;
+            this.login_button.Text = "Login";
+            this.login_button.UseVisualStyleBackColor = true;
+            // 
+            // Label_Password
+            // 
+            this.Label_Password.AutoSize = true;
+            this.Label_Password.Location = new System.Drawing.Point(427, 189);
+            this.Label_Password.Name = "Label_Password";
+            this.Label_Password.Size = new System.Drawing.Size(71, 16);
+            this.Label_Password.TabIndex = 12;
+            this.Label_Password.Text = "PassWord";
+            // 
+            // TxtBox_Password
+            // 
+            this.TxtBox_Password.Location = new System.Drawing.Point(430, 221);
+            this.TxtBox_Password.Name = "TxtBox_Password";
+            this.TxtBox_Password.Size = new System.Drawing.Size(246, 22);
+            this.TxtBox_Password.TabIndex = 11;
+            // 
+            // Label_UserName
+            // 
+            this.Label_UserName.AutoSize = true;
+            this.Label_UserName.Location = new System.Drawing.Point(427, 92);
+            this.Label_UserName.Name = "Label_UserName";
+            this.Label_UserName.Size = new System.Drawing.Size(76, 16);
+            this.Label_UserName.TabIndex = 10;
+            this.Label_UserName.Text = "User Name";
+            // 
+            // TxtBoxt_UserName
+            // 
+            this.TxtBoxt_UserName.Location = new System.Drawing.Point(430, 124);
+            this.TxtBoxt_UserName.Name = "TxtBoxt_UserName";
+            this.TxtBoxt_UserName.Size = new System.Drawing.Size(246, 22);
+            this.TxtBoxt_UserName.TabIndex = 9;
+            // 
+            // RegisterAdmin
+            // 
+            this.RegisterAdmin.AutoSize = true;
+            this.RegisterAdmin.Location = new System.Drawing.Point(237, 57);
+            this.RegisterAdmin.Name = "RegisterAdmin";
+            this.RegisterAdmin.Size = new System.Drawing.Size(99, 16);
+            this.RegisterAdmin.TabIndex = 8;
+            this.RegisterAdmin.Text = "Register Admin";
+            // 
+            // FormAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.register_button);
+            this.Controls.Add(this.login_button);
+            this.Controls.Add(this.Label_Password);
+            this.Controls.Add(this.TxtBox_Password);
+            this.Controls.Add(this.Label_UserName);
+            this.Controls.Add(this.TxtBoxt_UserName);
+            this.Controls.Add(this.RegisterAdmin);
+            this.Name = "FormAdmin";
             this.Text = "FormAdmin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button register_button;
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.Label Label_Password;
+        private System.Windows.Forms.TextBox TxtBox_Password;
+        private System.Windows.Forms.Label Label_UserName;
+        private System.Windows.Forms.TextBox TxtBoxt_UserName;
+        private System.Windows.Forms.Label RegisterAdmin;
     }
 }
--- WindowsFormsApp3/FormAdmin.cs
+++ WindowsFormsApp3/FormAdmin.cs
@@ -7,14 +7,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp3
 {
     public partial class FormAdmin : Form
     {
+
+        private SqlCommand cmd;
+        private DataSet ds;
+        private SqlDataAdapter da;
+        private SqlDataReader rd;
+
+        Koneksi Konn = new Koneksi();
+
+
         public FormAdmin()
         {
             InitializeComponent();
         }
+
+
+        private void RegisterAdmin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label_UserName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtBoxt_UserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label_Password_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtBox_Password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_button_Click(object sender, EventArgs e)
+        {
+            string username, u_password;
+
+            username = TxtBoxt_UserName.Text;
+            u_password = TxtBox_Password.Text;
+
+
+            SqlConnection conn = Konn.GetConn();
+            conn.Open();
+        }
+
+        private void register_button_Click(object sender, EventArgs e)
+        {
+            if (TxtBoxt_UserName.Text.Trim() == "" ^ TxtBox_Password.Text.Trim() == "")
+            {
+                MessageBox.Show("Data tidak lengkap");
+            }
+            else
+            {
+                SqlConnection conn = Konn.GetConn();
+                conn.Open();
+                try
+                {
+                    cmd = new SqlCommand("Insert into account_table values ('" + TxtBoxt_UserName.Text + "','" + TxtBox_Password.Text + "')", conn);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Akun berhasil di register");
+                    TxtBoxt_UserName.Clear();
+                    TxtBox_Password.Clear();
+                    TxtBoxt_UserName.Focus();
+                }
+                catch (Exception X)
+                {
+                    MessageBox.Show(X.ToString());
+                }
+            }
+        }
+
     }
 }
